d590dd92fa9bdbd13800038d45cebb8f1750fd59
David Levi <72753366+david-levi-student@users.noreply.github.com>
2022-02-16 11:39:20 +0000

    Add files via upload

--- Challenges/Challenges/Challenges.cpp
+++ Challenges/Challenges/Challenges.cpp
@@ -5,16 +5,17 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int num = 1;
+
+	switch (num)
+	{
+	case 0:
+		std::cout << "0" << std::endl;
+		break;
+	case 1:
+		std::cout << "1" << std::endl;
+		break;
+	case 2:
+		std::cout << "2" << std::endl;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
